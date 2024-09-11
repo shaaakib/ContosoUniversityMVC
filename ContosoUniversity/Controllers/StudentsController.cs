e6642ff95eaf1df713b4d74e124981dde68edde1
shaaakib <shakibhossainmuhammad@gmail.com>
2024-09-11 16:27:44 +0600

    Add paging to Students Index

--- ContosoUniversity/Controllers/StudentsController.cs
+++ ContosoUniversity/Controllers/StudentsController.cs
@@ -14,12 +14,28 @@ namespace ContosoUniversity.Controllers
             _context = context;
         }
 
-        public async Task<IActionResult> Index( string sortOrder)
+        public async Task<IActionResult> Index( string sortOrder,string currentFilter, string searchString,
+        int? pageNumber)
         {
+            ViewData["CurrentSort"] = sortOrder;
             ViewData["NameSortParm"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
             ViewData["DateSortParm"] = sortOrder == "Date" ? "date_desc" : "Date";
-            var students = from s in _context.Students
-                           select s;
+            ViewData["CurrentFilter"] = searchString;
+            var students = from s in _context.Students select s;
+
+            if (searchString != null)
+            {
+                pageNumber = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+            
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                students = students.Where(s => s.Name.Contains(searchString));
+            }
             switch (sortOrder)
             {
                 case "name_desc":
@@ -28,14 +44,13 @@ namespace ContosoUniversity.Controllers
                 case "Date":
                     students = students.OrderBy(s => s.EnrollmentDate);
                     break;
-                case "date_desc":
-                    students = students.OrderByDescending(s => s.EnrollmentDate);
-                    break;
                 default:
                     students = students.OrderBy(s => s.Name);
                     break;
             }
-            return View(await students.AsNoTracking().ToListAsync());
+
+            int pageSize = 3;
+            return View(await PaginatedList<Student>.CreateAsync(students.AsNoTracking(), pageNumber ?? 1, pageSize));
         }
 
         public async Task<IActionResult> Details(int?id)
